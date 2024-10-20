a4ef52eefda127b253040c311c63cbba65a09638
Timofei Tsypyshev <ttsypyshev01@gmail.com>
2024-10-21 00:30:11 +0300

    add save with code
    although it is forbidden, I had to use JS, since it is impossible to output map[fileID]code in my architecture in another way

--- src/backend/http.go
+++ src/backend/http.go
@@ -21,32 +21,32 @@ func (app *App) SetupRoutes(r *gin.Engine) {
 func (app *App) handleHome(c *gin.Context) {
 	langID, err := parseQueryParam(c, "langID")
 	if err != nil {
-		handleError(c, http.StatusBadRequest, errors.New("invalid langID"), err)
+		handleError(c, http.StatusBadRequest, errors.New("[err] invalid langID"), err)
 		return
 	}
 
 	status, err := parseQueryParam(c, "status")
 	if err != nil {
-		handleError(c, http.StatusBadRequest, errors.New("invalid status"), err)
+		handleError(c, http.StatusBadRequest, errors.New("[err] invalid status"), err)
 		return
 	}
 
 	query := c.Query("langname")
 	filteredLangs, err := app.getFilteredLangs(query)
 	if err != nil {
-		handleError(c, http.StatusNotFound, errors.New("failed to retrieve language information"), err)
+		handleError(c, http.StatusNotFound, errors.New("[err] failed to retrieve language information"), err)
 		return
 	}
 
 	count, err := app.CountFiles(app.userID)
 	if err != nil {
-		handleError(c, http.StatusNotFound, errors.New("project was not created"), err)
+		handleError(c, http.StatusNotFound, errors.New("[err] project was not created"), err)
 		return
 	}
 
 	projectID, err := findLastDraft(app, app.userID)
 	if err != nil {
-		handleError(c, http.StatusNotFound, errors.New("project was not created"), err)
+		handleError(c, http.StatusNotFound, errors.New("[err] project was not created"), err)
 		return
 	}
 
@@ -65,13 +65,13 @@ func (app *App) handleInfo(c *gin.Context) {
 	idStr := c.Param("id")
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
-		handleError(c, http.StatusNotFound, errors.New("invalid language ID"), err)
+		handleError(c, http.StatusNotFound, errors.New("[err] invalid language ID"), err)
 		return
 	}
 
 	info, err := app.GetLangByID(id)
 	if err != nil {
-		handleError(c, http.StatusNotFound, errors.New("language information not available"), err)
+		handleError(c, http.StatusNotFound, errors.New("[err] language information not available"), err)
 		return
 	}
 
@@ -86,32 +86,33 @@ func (app *App) handleApp(c *gin.Context) {
 	idStr := c.Param("id")
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
-		handleError(c, http.StatusNotFound, errors.New("invalid project ID"), err)
+		handleError(c, http.StatusNotFound, errors.New("[err] invalid project ID"), err)
 		return
 	}
 
 	project, err := app.GetProjectByID(id)
 	if err != nil {
-		handleError(c, http.StatusNotFound, errors.New("failed to retrieve language information"), err)
+		handleError(c, http.StatusNotFound, errors.New("[err] failed to retrieve project information"), err)
 		return
 	}
 
 	files, err := app.GetFilesForProject(id)
 	if err != nil {
-		handleError(c, http.StatusNotFound, errors.New("failed to retrieve project files"), err)
+		handleError(c, http.StatusNotFound, errors.New("[err] failed to retrieve project files"), err)
 		return
 	}
+
 	langs, err := app.GetLangs()
 	if err != nil {
-		handleError(c, http.StatusNotFound, errors.New("failed to retrieve language information"), err)
+		handleError(c, http.StatusNotFound, errors.New("[err] failed to retrieve language information"), err)
 		return
 	}
 
 	c.HTML(http.StatusOK, "applications.tmpl", gin.H{
-		"Title": "Project",
+		"Title":   "Project",
 		"Project": project,
-		"Files": files,
-		"Langs": langs,
+		"Files":   files,
+		"Langs":   langs,
 	})
 }
 
@@ -122,25 +123,25 @@ type RequestAdd struct {
 
 func (app *App) handleAddService(c *gin.Context) {
 	var req RequestAdd
-
 	if err := c.ShouldBind(&req); err != nil {
-		handleError(c, http.StatusNotFound, errors.New("invalid data format"), err)
+		handleError(c, http.StatusBadRequest, errors.New("[err] invalid data format"), err)
 		return
 	}
+	log.Printf("[info] AddService called : IDUser=%d, IDLang=%d", req.IDUser, req.IDLang)
 
 	projectID, err := createDraft(app, req.IDUser)
 	if err != nil {
-		handleError(c, http.StatusNotFound, errors.New("error creating project"), err)
+		handleError(c, http.StatusNotFound, errors.New("[err] error creating project"), err)
 		return
 	}
 
 	if err := app.AddFile(projectID, req.IDLang, req.IDUser); err != nil {
-		log.Printf("INFO: Navigating to home page at URL: {URL}/home?langID=%d&status=%d", req.IDLang, 2)
+		log.Printf("[info] Redirecting to home page at URL: /home?langID=%d&status=%d", req.IDLang, 2)
 		c.Redirect(http.StatusSeeOther, fmt.Sprintf("/home?langID=%d&status=%d", req.IDLang, 2))
 		return
 	}
 
-	log.Printf("INFO: Navigating to home page at URL: {URL}/home?langID=%d&status=%d", req.IDLang, 1)
+	log.Printf("[info] Redirecting to home page at URL: /home?langID=%d&status=%d", req.IDLang, 1)
 	c.Redirect(http.StatusSeeOther, fmt.Sprintf("/home?langID=%d&status=%d", req.IDLang, 1))
 }
 
@@ -152,22 +153,22 @@ type RequestUpdate struct {
 
 func (app *App) handleUpdateProject(c *gin.Context) {
 	var req RequestUpdate
-
 	if err := c.ShouldBind(&req); err != nil {
-		handleError(c, http.StatusNotFound, errors.New("invalid data format"), err)
+		handleError(c, http.StatusBadRequest, errors.New("[err] invalid data format"), err)
 		return
 	}
+	log.Printf("[info] UpdateProject called: IDProject=%d, Status=%d, FileCodes=%v", req.IDProject, req.Status, req.FileCodes)
 
 	if err := app.UpdateProjectStatus(req.IDProject, req.Status); err != nil {
-		handleError(c, http.StatusNotFound, errors.New("failed to update project status"), err)
+		handleError(c, http.StatusNotFound, errors.New("[err] failed to update project status"), err)
 		return
 	}
 
 	if err := app.UpdateFilesCode(req.FileCodes); err != nil {
-		handleError(c, http.StatusNotFound, errors.New("failed to update file"), err)
+		handleError(c, http.StatusNotFound, errors.New("[err] failed to update file"), err)
 		return
 	}
 
-	log.Printf("INFO: Navigating to home page at URL: {URL}/home")
+	log.Printf("[info] Redirecting to home page at URL: /home")
 	c.Redirect(http.StatusSeeOther, "/home")
 }
